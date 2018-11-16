dd7d9dd48b127fcf44215f53e6fd97041926c4c1
Tyson Levy <tjl3553@cs.rit.edu>
2018-11-16 02:54:16 -0500

    falling and stacking

--- src/Bits/Bit.py
+++ src/Bits/Bit.py
@@ -2,7 +2,7 @@ import pygame
 
 class Bit:
 
-    def __init__(self, x, y, fall, display):
+    def __init__(self, x, y, fall, display, color=(255, 255, 255)):
         self.x = x
         self.y = y
         self.dx = 0
@@ -10,12 +10,33 @@ class Bit:
         self.fall = fall
         self.size = (1, 1)
         self.display = display
+        self.color = color
+        self.moved = False
 
-    def tick(self):
+    def tick(self, bits):
+        bits[self.x][self.y] = None
         if self.fall:
             self.dy += 1
         self.x += self.dx
         self.y += self.dy
+        if self.y >= 600 - self.size[1]:
+            self.y = 600 - self.size[1]
+            self.dy = 0
+            if self.x < 0:
+                self.x = 0
+                self.dx = 0
+            if self.x > 800 - self.size[0]:
+                self.x = 800 - self.size[0]
+                self.dx = 0
+
+        while bits[self.x][self.y] is not None:
+            self.y -= 1
+            self.dy = 0
+
+        bits[self.x][self.y] = self
+
+
+
 
     def show(self):
-        pygame.draw.rect(self.display, (255,255,255), [self.x, self.y, self.size[0], self.size[1]])
+        pygame.draw.rect(self.display, self.color, [self.x, self.y, self.size[0], self.size[1]])
--- src/main.py
+++ src/main.py
@@ -1,22 +1,26 @@
 import pygame
 from Bits.Bit import *
 
-black = (0,0,0)
-white = (255,255,255)
+black = (0, 0, 0)
+white = (255, 255, 255)
 
 
 def main():
     pygame.init()
-    GameDisplay = pygame.display.set_mode((800,600))
+    GameDisplay = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("BitGame")
     clock = pygame.time.Clock()
     gameOver = False
     down = False
 
     bits = []
+    for i in range(0, 800):
+        bits += [[]]
+        for j in range(0, 600):
+            bits[i] += [None]
 
     while not gameOver:
-        GameDisplay.fill((0,0,0))
+        GameDisplay.fill(black)
         for event in pygame.event.get():
             mouse = pygame.mouse.get_pos()
             if event.type == pygame.QUIT:
@@ -26,16 +30,25 @@ def main():
             if event.type == pygame.MOUSEBUTTONUP:
                 down = False
         if down:
-            bits += [Bit(mouse[0], mouse[1], True, GameDisplay)]
+            if bits[mouse[0]][mouse[1]] is None:
+                bits[mouse[0]][mouse[1]] = Bit(mouse[0], mouse[1], True, GameDisplay)
+            #down = False
 
-        for bit in bits:
-                bit.show()
-                bit.tick()
+        for bitList in bits:
+            for bit in bitList:
+                if bit is not None:
+                    if not bit.moved:
+                        bit.show()
+                        bit.tick(bits)
+                        bit.moved = True
+            for bit in bitList:
+                if bit is not None:
+                    bit.moved = False
 
 
 
         pygame.display.update()
-        clock.tick(60)
+        clock.tick(30)
 
 
 if __name__ == "__main__":
